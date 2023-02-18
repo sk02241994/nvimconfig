e9924edae3f651fd9e22faff88237a0c4eb8ae49
sk02241994 <skaradkar57@gmail.com>
2023-02-18 11:11:05 +0530

    moved all the content to one file

--- init.lua
+++ init.lua
@@ -147,6 +147,10 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('n', '<c-l>', '<C-w>l', {noremap = true})
+vim.keymap.set('n', '<c-k>', '<C-w>k', {noremap = true})
+vim.keymap.set('n', '<c-j>', '<C-w>j', {noremap = true})
+vim.keymap.set('n', '<c-h>', '<C-w>h', {noremap = true})
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
@@ -323,7 +327,7 @@ local on_attach = function(_, bufnr)
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+  nmap('<C-s>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
   -- Lesser used LSP functionality
   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
@@ -375,7 +379,7 @@ local servers = {
     filetypes = {"java"},
     single_file_support = true,
   },
-  sumneko_lua = {
+  lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
@@ -551,3 +555,110 @@ require('symbols-outline').setup({
   }
 })
 vim.keymap.set("n", "<F2>", "<cmd>SymbolsOutline<cr>", {})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "java",
+  callback = function()
+    local jdtls_path = vim.fn.stdpath('data') .. "/mason/packages/jdtls"
+    local path_to_lsp_server = jdtls_path .. "/config_linux"
+    local path_to_plugins = jdtls_path .. "/plugins/"
+    local path_to_jar = path_to_plugins .. "org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar"
+    local lombok_path = jdtls_path .. "/lombok.jar"
+
+    local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
+    local root_dir = require("jdtls.setup").find_root(root_markers)
+    if root_dir == "" then
+      return
+    end
+
+    local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+    local workspace_dir = vim.fn.stdpath('data') .. '/site/java/workspace-root/' .. project_name
+    os.execute("mkdir -p " .. workspace_dir)
+
+    local config = {
+      cmd = {
+	'java',
+	'-Declipse.application=org.eclipse.jdt.ls.core.id1',
+	'-Dosgi.bundles.defaultStartLevel=4',
+	'-Declipse.product=org.eclipse.jdt.ls.core.product',
+	'-Dlog.protocol=true',
+	'-Dlog.level=ALL',
+	'-javaagent:' .. lombok_path,
+	'-Xms1g',
+	'--add-modules=ALL-SYSTEM',
+	'--add-opens', 'java.base/java.util=ALL-UNNAMED',
+	'--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+
+	'-jar', path_to_jar,
+	'-configuration', path_to_lsp_server,
+	'-data', workspace_dir,
+      },
+      root_dir = root_dir,
+      settings = {
+	java = {
+	  home = '/home/shubham/Language/java',
+	  eclipse = {
+	    downloadSources = true
+	  },
+	  maven = {
+	    downloadSources = true,
+	  },
+	  implementationsCodeLens = {
+	    enabled = true,
+	  },
+	  referencesCodeLens = {
+	    enabled = true,
+	  },
+	  references = {
+	    includeDecompiledSources = true,
+	  },
+	},
+	signatureHelp = { enabled = true },
+	completion = {
+	  favoriteStaticMembers = {
+	    "org.hamcrest.MatcherAssert.assertThat",
+	    "org.hamcrest.Matchers.*",
+	    "org.hamcrest.CoreMatchers.*",
+	    "org.junit.jupiter.api.Assertions.*",
+	    "java.util.Objects.requireNonNull",
+	    "java.util.Objects.requireNonNullElse",
+	    "org.mockito.Mockito.*",
+	  },
+	  importOrder = {
+	    "java",
+	    "javax",
+	    "com",
+	    "org"
+	  },
+	},
+	extendedClientCapabilities = extendedClientCapabilities,
+	sources = {
+	  organizeImports = {
+	    starThreshold = 9999,
+	    staticStarThreshold = 9999,
+	  },
+	},
+	codeGeneration = {
+	  toString = {
+	    template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+	  },
+	  useBlocks = true,
+	},
+
+      },
+      flags = {
+	allow_incremental_sync = true,
+      },
+init_options = {
+	bundles = {},
+      },
+
+    }
+
+    config['on_attach'] = function(client, bufnr)
+      require'lsp-keybind'.on_attach(client, bufnr)
+    end
+
+    require('jdtls').start_or_attach(config)
+  end
+})

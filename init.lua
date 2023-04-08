a1ebfdf8c7711eb9f2c8d1081e67cfe28b9226b0
Shubham K <53286173+sk02241994@users.noreply.github.com>
2023-04-08 09:27:43 +0530

    Windows configuration
    Windows configuration for neovim

--- init.lua
+++ init.lua
@@ -15,30 +15,30 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 require("lazy").setup({
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    dependencies = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
-      {'williamboman/mason.nvim'},           -- Optional
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},         -- Required
-      {'hrsh7th/cmp-nvim-lsp'},     -- Required
-      {'hrsh7th/cmp-buffer'},       -- Optional
-      {'hrsh7th/cmp-path'},         -- Optional
-      {'saadparwaiz1/cmp_luasnip'}, -- Optional
-      {'hrsh7th/cmp-nvim-lua'},     -- Optional
-      {'hrsh7th/cmp-cmdline'},
-
-      -- Snippets
-      {'L3MON4D3/LuaSnip'},             -- Required
-      {'rafamadriz/friendly-snippets'}, -- Optional
-      {'j-hui/fidget.nvim'}
-    }
-  },
+--  {
+--    'VonHeikemen/lsp-zero.nvim',
+--    branch = 'v1.x',
+--    dependencies = {
+--      -- LSP Support
+--      {'neovim/nvim-lspconfig'},             -- Required
+--      {'williamboman/mason.nvim'},           -- Optional
+--      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+--
+--      -- Autocompletion
+--      {'hrsh7th/nvim-cmp'},         -- Required
+--      {'hrsh7th/cmp-nvim-lsp'},     -- Required
+--      {'hrsh7th/cmp-buffer'},       -- Optional
+--      {'hrsh7th/cmp-path'},         -- Optional
+--      {'saadparwaiz1/cmp_luasnip'}, -- Optional
+--      {'hrsh7th/cmp-nvim-lua'},     -- Optional
+--      {'hrsh7th/cmp-cmdline'},
+--
+--      -- Snippets
+--      {'L3MON4D3/LuaSnip'},             -- Required
+--      {'rafamadriz/friendly-snippets'}, -- Optional
+--      {'j-hui/fidget.nvim'}
+--    }
+--  },
   { 'nvim-lualine/lualine.nvim' },
   { 'tpope/vim-sleuth' },
   { 'nvim-telescope/telescope.nvim', branch = '0.1.x',  dependencies={'nvim-lua/plenary.nvim' } },
@@ -50,7 +50,7 @@ require("lazy").setup({
     "windwp/nvim-autopairs",
     config = function() require("nvim-autopairs").setup {} end
   },
-  {'mfussenegger/nvim-jdtls'},
+--  {'mfussenegger/nvim-jdtls'},
   {'folke/tokyonight.nvim'},
   {
 		'nvim-tree/nvim-tree.lua',
@@ -78,9 +78,9 @@ vim.keymap.set('n', '<C-j>', '<C-w>j', {noremap = true})
 vim.keymap.set('n', '<C-h>', '<C-w>h', {noremap = true})
 
 -- fidget
-require('fidget').setup()
+-- require('fidget').setup()
 -- mason
-require('mason').setup()
+-- require('mason').setup()
 -- lualine
 require('lualine').setup {
   options = {
@@ -203,277 +203,274 @@ require('nvim-treesitter.configs').setup {
 
 -- lsp config
 
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
-local on_attach = function(_, bufnr)
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
-
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
-
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-  nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-s>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
-
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
-end
-
-
-local servers = {
-  clangd = {
-    cmd = { "clangd" },
-    filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-    single_file_support = true,
-  },
-  pyright = {
-    filetypes = {"python"},
-    settings ={
-      python = {
-        analysis = {
-          autoSearchPaths = true,
-          diagnosticMode = "workspace",
-          useLibraryCodeForTypes = true
-        }
-      },
-    },
-    single_file_support = true
-  },
-  -- rust_analyzer = {},
-  -- tsserver = {},
-  tsserver = {
-    filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
-    init_options = {hostInfo = "neovim"},
-  },
-
-  jdtls = {
-    filetypes = {"java"},
-    single_file_support = true,
-  },
-  lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-      diagnostics = {globals = {'vim'}}
-    },
-  },
-}
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
--- Setup mason so it can manage external tooling
-require('mason').setup()
-
--- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
-
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    })
-  end,
-}
-
--- nvim-cmp setup
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
-
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert {
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  },
-}
-
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-      { name = 'cmdline' }
-    })
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "java",
-  callback = function ()
-
-    local jdtls_path = vim.fn.stdpath('data') .. "/mason/packages/jdtls"
-    local path_to_lsp_server = jdtls_path .. "/config_linux"
-    local path_to_plugins = jdtls_path .. "/plugins/"
-    local path_to_jar = path_to_plugins .. "org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar"
-    local lombok_path = jdtls_path .. "/lombok.jar"
-
-    local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
-    local root_dir = require("jdtls.setup").find_root(root_markers)
-    if root_dir == "" then
-      return
-    end
-
-    local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-    local workspace_dir = vim.fn.stdpath('data') .. '/site/java/workspace-root/' .. project_name
-    os.execute("mkdir -p " .. workspace_dir)
-
-    local config = {
-      cmd = {
-	'java',
-	'-Declipse.application=org.eclipse.jdt.ls.core.id1',
-	'-Dosgi.bundles.defaultStartLevel=4',
-	'-Declipse.product=org.eclipse.jdt.ls.core.product',
-	'-Dlog.protocol=true',
-	'-Dlog.level=ALL',
-	'-javaagent:' .. lombok_path,
-	'-Xms1g',
-	'--add-modules=ALL-SYSTEM',
-	'--add-opens', 'java.base/java.util=ALL-UNNAMED',
-	'--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-
-	'-jar', path_to_jar,
-	'-configuration', path_to_lsp_server,
-	'-data', workspace_dir,
-      },
-      root_dir = root_dir,
-      settings = {
-	java = {
-	  home = '/home/shubham/Language/java',
-	  eclipse = {
-	    downloadSources = true
-	  },
-	  maven = {
-	    downloadSources = true,
-	  },
-	  implementationsCodeLens = {
-	    enabled = true,
-	  },
-	  referencesCodeLens = {
-	    enabled = true,
-	  },
-	  references = {
-	    includeDecompiledSources = true,
-	  },
-	},
-	signatureHelp = { enabled = true },
-	completion = {
-	  favoriteStaticMembers = {
-	    "org.hamcrest.MatcherAssert.assertThat",
-	    "org.hamcrest.Matchers.*",
-	    "org.hamcrest.CoreMatchers.*",
-	    "org.junit.jupiter.api.Assertions.*",
-	    "java.util.Objects.requireNonNull",
-	    "java.util.Objects.requireNonNullElse",
-	    "org.mockito.Mockito.*",
-	  },
-	  importOrder = {
-	    "java",
-	    "javax",
-	    "com",
-	    "org"
-	  },
-	},
-	extendedClientCapabilities = extendedClientCapabilities,
-	sources = {
-	  organizeImports = {
-	    starThreshold = 9999,
-	    staticStarThreshold = 9999,
-	  },
-	},
-	codeGeneration = {
-	  toString = {
-	    template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
-	  },
-	  useBlocks = true,
-	},
-
-      },
-      flags = {
-	allow_incremental_sync = true,
-      },
-init_options = {
-	bundles = {},
-      },
-
-    }
-
-    config['on_attach'] = function(client, bufnr)
-      require'lsp-keybind'.on_attach(client, bufnr)
-    end
-
-    require('jdtls').start_or_attach(config)
-  end
-})
+-- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+-- vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+-- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+-- local on_attach = function(_, bufnr)
+--   local nmap = function(keys, func, desc)
+--     if desc then
+--       desc = 'LSP: ' .. desc
+--     end
+-- 
+--     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+--   end
+-- 
+--   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+--   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+-- 
+--   nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+--   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+--   nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+--   nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+--   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+--   nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+-- 
+--   -- See `:help K` for why this keymap
+--   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+--   nmap('<C-s>', vim.lsp.buf.signature_help, 'Signature Documentation')
+-- 
+--   -- Lesser used LSP functionality
+--   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+--   nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+--   nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+--   nmap('<leader>wl', function()
+--     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+--   end, '[W]orkspace [L]ist Folders')
+-- 
+--   -- Create a command `:Format` local to the LSP buffer
+--   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+--     vim.lsp.buf.format()
+--   end, { desc = 'Format current buffer with LSP' })
+-- end
+-- 
+-- 
+-- local servers = {
+--   -- gopls = {},
+--   pyright = {
+--     filetypes = {"python"},
+--     settings ={
+--       python = {
+--         analysis = {
+--           autoSearchPaths = true,
+--           diagnosticMode = "workspace",
+--           useLibraryCodeForTypes = true
+--         }
+--       },
+--     },
+--     single_file_support = true
+--   },
+--   -- rust_analyzer = {},
+--   -- tsserver = {},
+--   tsserver = {
+--     filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+--     init_options = {hostInfo = "neovim"},
+--   },
+-- 
+--   jdtls = {
+--     filetypes = {"java"},
+--     single_file_support = true,
+--   },
+--   lua_ls = {
+--     Lua = {
+--       workspace = { checkThirdParty = false },
+--       telemetry = { enable = false },
+--       diagnostics = {globals = {'vim'}}
+--     },
+--   },
+-- }
+-- 
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+-- 
+-- -- Setup mason so it can manage external tooling
+-- require('mason').setup()
+-- 
+-- -- Ensure the servers above are installed
+-- local mason_lspconfig = require 'mason-lspconfig'
+-- 
+-- mason_lspconfig.setup {
+--   ensure_installed = vim.tbl_keys(servers),
+-- }
+-- 
+-- mason_lspconfig.setup_handlers {
+--   function(server_name)
+--     require('lspconfig')[server_name].setup({
+--       capabilities = capabilities,
+--       on_attach = on_attach,
+--       settings = servers[server_name],
+--     })
+--   end,
+-- }
+-- 
+-- -- nvim-cmp setup
+-- local cmp = require 'cmp'
+-- local luasnip = require 'luasnip'
+-- 
+-- cmp.setup {
+--   snippet = {
+--     expand = function(args)
+--       luasnip.lsp_expand(args.body)
+--     end,
+--   },
+--   mapping = cmp.mapping.preset.insert {
+--     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+--     ['<C-f>'] = cmp.mapping.scroll_docs(4),
+--     ['<C-Space>'] = cmp.mapping.complete(),
+--     ['<CR>'] = cmp.mapping.confirm {
+--       behavior = cmp.ConfirmBehavior.Replace,
+--       select = true,
+--     },
+--     ['<Tab>'] = cmp.mapping(function(fallback)
+--       if cmp.visible() then
+--         cmp.select_next_item()
+--       elseif luasnip.expand_or_jumpable() then
+--         luasnip.expand_or_jump()
+--       else
+--         fallback()
+--       end
+--     end, { 'i', 's' }),
+--     ['<S-Tab>'] = cmp.mapping(function(fallback)
+--       if cmp.visible() then
+--         cmp.select_prev_item()
+--       elseif luasnip.jumpable(-1) then
+--         luasnip.jump(-1)
+--       else
+--         fallback()
+--       end
+--     end, { 'i', 's' }),
+--   },
+--   sources = {
+--     { name = 'nvim_lsp' },
+--     { name = 'luasnip' },
+--   },
+-- }
+-- 
+-- cmp.setup.cmdline({ '/', '?' }, {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   sources = {
+--     { name = 'buffer' }
+--   }
+-- })
+-- 
+-- -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+-- cmp.setup.cmdline(':', {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   sources = cmp.config.sources({
+--     { name = 'path' }
+--   }, {
+--       { name = 'cmdline' }
+--     })
+-- })
+-- 
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "java",
+--   callback = function ()
+--     local jdtls_dir = vim.fn.stdpath('data') .. '\\mason\\packages\\jdtls'
+--     local config_dir = jdtls_dir .. '\\config_win'
+--     local plugins_dir = jdtls_dir .. '\\plugins\\'
+--     local path_to_jar = plugins_dir .. 'org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar'
+--     local path_to_lombok = jdtls_dir .. '/lombok.jar'
+--     
+--     local root_marker = {'.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle'}
+--     local root_dir = require('jdtls.setup').find_root(root_marker)
+--     
+--     if root_dir == '' then
+--       return
+--     end
+--     
+--     local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+--     local workspace_dir = vim.fn.stdpath('data') .. 'site/java/workspace-root' .. project_name
+--     os.execute('mkdir' .. workspace_dir)
+--     
+--     local config = {
+--       cmd = {
+--         'C:/Users/altres/AppData/Local/Programs/java18/bin/java',
+--         '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+--         '-Dosgi.bundles.defaultStartLevel=4',
+--         '-Declipse.product=org.eclipse.jdt.ls.core.product',
+--         '-Dlog.protocol=true',
+--         '-Dlog.level=ALL',
+--         '-javaagent:' .. path_to_lombok,
+--         '-Xms1g',
+--         '--add-modules=ALL-SYSTEM',
+--         '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+--         '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+--         '-jar', path_to_jar,
+--         '-configuration', config_dir,
+--         '-data', workspace_dir
+--       },
+--       root_dir = root_dir,
+--       settings = {
+--         java = {
+--           home = 'C:/Users/altres/AppData/Local/Programs/java',
+--           eclipse = {downloadSources = true},
+--           maven = {downloadSources = true},
+--           configuration = {
+--             updateBuildConfiguration = 'interactive',
+--             runtimes = {
+--               name = 'JavaSE-8',
+--               path = 'C:/Users/altres/AppData/Local/Programs/java8'
+--             },
+--             {
+--               name = 'JavaSE-18',
+--               path = 'C:/Users/altres/AppData/Local/Programs/java18'
+--             }
+--           },
+--           implementationsCodeLens = {enabled = true},
+--           referencesCodeLens = {enabled = true},
+--           refernces = {includeDecompiledSources = true},
+--         },
+--         signatureHelp = {enabled = true},
+--         completion = {
+--           favoriteStaticMembers = {
+--             "org.hamcrest.MatcherAssert.assertThat",
+--             "org.hamcrest.Matchers.*",
+--             "org.hamcrest.CoreMatchers.*",
+--             "org.junit.jupiter.api.Assertions.*",
+--             "java.util.Objects.requireNonNull",
+--             "java.util.Objects.requireNonNullElse",
+--             "org.mockito.Mockito.*",
+--           },
+--           importOrder = {
+--             "java",
+--             "javax",
+--             "com",
+--             "org"
+--           },
+--         },
+--         sources = {
+--           organizeImports = {
+--             starThreshold = 9999,
+--             staticStarThreshold = 9999,
+--           },
+--         },
+--         codeGeneration = {
+--           toString = {
+--             template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+--           },
+--           useBlocks = true,
+--         },
+--       }
+--     }
+--     
+--     config['on_attach'] = function(client, bufnr)
+--       on_attach(client, bufnr)
+--     end
+--     
+--     require('jdtls').start_or_attach(config)
+--     
+--   end
+-- })
+-- 
+-- require('lspconfig').clangd.setup {
+--   capabilities = capabilities,
+--   on_attach = on_attach,
+--   cmd = { "clangd" },
+--   filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+--   single_file_support = true,
+-- }
+-- 
 
 --colorscheme
 vim.cmd[[colorscheme tokyonight]]

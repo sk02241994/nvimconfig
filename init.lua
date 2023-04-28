52f046349c46ba442cb5864154b0e16698211914
Shubham K <53286173+sk02241994@users.noreply.github.com>
2023-04-28 09:23:42 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -17,6 +17,9 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
+vim.opt.colorcolumn="120"
+vim.opt.cursorcolumn=true
+vim.opt.cursorline=true
 
 require("lazy").setup({
     {

94a210149a991d06dd97aa8dd382cabb1163421a
sk02241994 <skaradkar57@gmail.com>
2024-02-29 20:58:03 +0530

    Updating lspconfig

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -53,7 +53,7 @@ lspconfig.kotlin_language_server.setup({
 
 local root_marker = {'.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle', 'classes', 'lib'}
 lspconfig.jdtls.setup({
-  cmd = {vim.fn.stdpath('data') .. '/mason/packages/jdtls/bin/jdtls.bat'},
+  cmd = {vim.fn.stdpath('data') .. '/mason/packages/jdtls/bin/jdtls'},
   filetypes = {'java'},
   capabilities = capabilities,
   on_attach = on_attach,

99ac879288cbb6a30f15ee870b0cb79018bbd405
sk02241994 <skaradkar57@gmail.com>
2024-08-06 07:07:32 +0530

    Updating java lsp

--- init.lua
+++ init.lua
@@ -342,93 +342,88 @@ java ^
 -configuration path\to\jdtls\config_linux\ \
 -data "$TEMP\$1"
 ]] --
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { 'java' },
-  callback = function()
-    local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-    local root_marker = { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle', 'classes', 'lib', 'settings.gradle',
-      'gradle.properties' }
-    local config = coq.lsp_ensure_capabilities({
-      cmd = { 'jdtls', '.workspace_' .. project_name },
-      init_options = {
-        bundles = {},
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local root_marker = { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' }
+lspconfig.jdtls.setup(coq.lsp_ensure_capabilities({
+  cmd = { 'jdtls', '.workspace_' .. project_name },
+  capabilities = capabilities,
+  single_file_support = true,
+  on_attach = function(_, bufnr)
+    vim.keymap.set('n', '<leader>joi', "<cmd>lua require('jdtls').organize_imports()<CR>",
+      { desc = 'Java Organize Imports', buffer = bufnr })
+    vim.keymap.set('n', '<leader>jtc', "<cmd>lua require('jdtls').test_class()<CR>",
+      { desc = 'Java test class', silent = true, buffer = bufnr })
+    vim.keymap.set('n', '<leader>jtnm', "<cmd>lua require('jdtls').test_nearest_method()<CR>",
+      { desc = 'Java test nearest method', silent = true, buffer = bufnr })
+    vim.keymap.set('n', '<leader>jev', "<cmd>lua require('jdtls').extract_variable_all()<CR>",
+      { desc = 'Java extract variable all', silent = true, buffer = bufnr })
+    vim.keymap.set('v', '<leader>jem', "<ESC><CR>lua require('jdtls').extract_method(true)<CR>",
+      { desc = 'Java extract method', buffer = bufnr })
+    vim.keymap.set('n', '<leader>jec', "<CR>lua require('jdtls').extract_constant()<CR>",
+      { desc = 'Java extract constant', buffer = bufnr })
+  end,
+  init_options = {
+    bundles = {},
+  },
+  root_dir = function ()
+    return require('jdtls.setup').find_root(root_marker)
+  end,
+  settings = {
+    java = {
+      signatureHelp = { enabled = true },
+      completion = {
+        favoriteStaticMembers = {
+          "org.hamcrest.MatcherAssert.assertThat",
+          "org.hamcrest.Matchers.*",
+          "org.hamcrest.CoreMatchers.*",
+          "org.junit.jupiter.api.Assertions.*",
+          "java.util.Objects.requireNonNull",
+          "java.util.Objects.requireNonNullElse",
+          "org.mockito.Mockito.*"
+        }
       },
-      root_dir = require('jdtls.setup').find_root(root_marker),
-      capabilities = capabilities,
-      settings = {
-        java = {
-          signatureHelp = { enabled = true },
-          completion = {
-            favoriteStaticMembers = {
-              "org.hamcrest.MatcherAssert.assertThat",
-              "org.hamcrest.Matchers.*",
-              "org.hamcrest.CoreMatchers.*",
-              "org.junit.jupiter.api.Assertions.*",
-              "java.util.Objects.requireNonNull",
-              "java.util.Objects.requireNonNullElse",
-              "org.mockito.Mockito.*"
-            }
+      eclipse = { downloadSources = true },
+      maven = { downloadSources = true },
+      referenceCodeLens = { enabled = true },
+      references = { includeDecompiledSources = true },
+      inlayHints = {
+        parameterNames = {
+          enabled = 'all'
+        }
+      },
+      extendedClientCapabilities = require('jdtls').extendedClientCapabilities,
+      configuration = {
+        updateBuildConfiguration = "interactive",
+        --[[runtimes = {
+          {
+            name = 'JavaSE-1.8',
+            path = 'D:/java/java8',
+            default = true,
           },
-          eclipse = { downloadSources = true },
-          maven = { downloadSources = true },
-          referenceCodeLens = { enabled = true },
-          references = { includeDecompiledSources = true },
-          inlayHints = {
-            parameterNames = {
-              enabled = 'all'
-            }
+          {
+            name = 'JavaSE-21',
+            path = 'D:/java/java21'
           },
-          extendedClientCapabilities = require('jdtls').extendedClientCapabilities,
-          configuration = {
-            updateBuildConfiguration = "interactive",
-            --[[runtimes = {
-              {
-                name = 'JavaSE-1.8',
-                path = 'D:/java/java8',
-                default = true,
-              },
-              {
-                name = 'JavaSE-21',
-                path = 'D:/java/java21'
-              },
-              {
-                name = 'JavaSE-22',
-                path = 'D:/java/java22'
-              },
-            },]] --
+          {
+            name = 'JavaSE-22',
+            path = 'D:/java/java22'
           },
-          sources = {
-            organizeImports = {
-              starThreshold = 9999,
-              staticStarThreshold = 9999,
-            }
-          },
-          codeGeneration = {
-            toString = {
-              template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
-            }
-          },
-        },
-      }
-    })
-    config.on_attach = function(_, bufnr)
-      vim.keymap.set('n', '<leader>joi', "<cmd>lua require('jdtls').organize_imports()<CR>",
-        { desc = 'Java Organize Imports', buffer = bufnr })
-      vim.keymap.set('n', '<leader>jtc', "<cmd>lua require('jdtls').test_class()<CR>",
-        { desc = 'Java test class', silent = true, buffer = bufnr })
-      vim.keymap.set('n', '<leader>jtnm', "<cmd>lua require('jdtls').test_nearest_method()<CR>",
-        { desc = 'Java test nearest method', silent = true, buffer = bufnr })
-      vim.keymap.set('n', '<leader>jev', "<cmd>lua require('jdtls').extract_variable_all()<CR>",
-        { desc = 'Java extract variable all', silent = true, buffer = bufnr })
-      vim.keymap.set('v', '<leader>jem', "<ESC><CR>lua require('jdtls').extract_method(true)<CR>",
-        { desc = 'Java extract method', buffer = bufnr })
-      vim.keymap.set('n', '<leader>jec', "<CR>lua require('jdtls').extract_constant()<CR>",
-        { desc = 'Java extract constant', buffer = bufnr })
-    end
-    -- require('jdtls').set_runtime('JavaSE-1.8')
-    require('jdtls').start_or_attach(config)
-  end
-})
+        },]]--
+      },
+      sources = {
+        organizeImports = {
+          starThreshold = 9999,
+          staticStarThreshold = 9999,
+        }
+      },
+      codeGeneration = {
+        toString = {
+          template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
+        }
+      },
+    },
+  }
+}))
 
 lspconfig.rust_analyzer.setup(coq.lsp_ensure_capabilities({
   cmd = { 'rust-analyzer' },

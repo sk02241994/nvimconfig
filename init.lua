85db264c4929db2d2d44b1c573b6f96587e05348
sk02241994 <skaradkar57@gmail.com>
2024-09-25 20:56:54 +0530

    Adding telescope

--- init.lua
+++ init.lua
@@ -1,3 +1,23 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+})
+
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 vim.g.maplocalleader = ' '
 vim.opt.tabstop = 2
@@ -101,3 +121,24 @@ if file then
   vim.cmd('luafile ' .. config_file)
 file:close()
 end
+
+require('telescope').setup {
+  defaults = require('telescope.themes').get_dropdown {
+    previewer = false,
+    color_devicons = true,
+    layout_strategy = "horizontal",
+    prompt_prefix = "   ",
+    layout_config = {
+      horizontal = {
+        width = 0.99,
+        prompt_position = "top",
+        preview_width = 0.0,
+      },
+    },
+  },
+}
+
+vim.keymap.set('n', '<leader>t', "<cmd>Telescope<cr>", { desc = 'Open telescope' })
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = 'Find file' })
+vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = 'Show buffers' })
+vim.keymap.set('n', '<leader>fw', require('telescope.builtin').live_grep, { desc = 'Live grep' })

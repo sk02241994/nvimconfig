c81acaa1535e56aa8f139ddd579ed6cd7b363249
sk02241994 <skaradkar57@gmail.com>
2025-04-28 11:08:56 +1000

    Adding telescope and keymaps

--- init.lua
+++ init.lua
@@ -1,3 +1,34 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim', "nvim-telescope/telescope-live-grep-args.nvim" }
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {}
+  },
+  { 'skywind3000/asyncrun.vim' },
+})
+
+
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 vim.g.maplocalleader = ' '
 vim.opt.tabstop = 2
@@ -39,10 +70,10 @@ vim.keymap.set('n', '<a-up>', '<c-w>+', {desc = 'resize height increase'})
 vim.keymap.set('n', '<a-down>', '<c-w>-', {desc = 'resize height decrease'})
 vim.keymap.set('n', '<a-left>', '<c-w>>', {desc = 'resize width increase'})
 vim.keymap.set('n', '<a-right>', '<c-w><', {desc = 'resiez width decrease'})
-vim.keymap.set('n', '<leader>fb', ':buffers<cr>:buffer<space>', { desc = 'Show buffers' })
+-- vim.keymap.set('n', '<leader>fb', ':buffers<cr>:buffer<space>', { desc = 'Show buffers' })
 vim.keymap.set('n', "<leader>fg", "<cmd>grep -s -w \"<cword>\"<cr>", {desc = "Find word under cursor"})
-vim.keymap.set('n', '<leader>m', ':marks<cr>:\'', { desc = 'Show marks' })
-vim.keymap.set('n', '<leader>v', ':registers<cr>', { desc = 'Show marks' })
+-- vim.keymap.set('n', '<leader>m', ':marks<cr>:\'', { desc = 'Show marks' })
+-- vim.keymap.set('n', '<leader>v', ':registers<cr>', { desc = 'Show marks' })
 vim.keymap.set('n', '<F4>', "<cmd>cn<cr>", { desc = 'Quickfix next' })
 vim.keymap.set('n', '<F5>', "<cmd>cp<cr>", { desc = 'Quickfix previous' })
 vim.keymap.set('n', '<F2>', function() 
@@ -107,3 +138,50 @@ end
 
 -- vim.opt.runtimepath:append("~/.config/nvim/custom_files/*")
 -- vim.cmd('luafile ' .. '~/.config/nvim/custom_files/custom_config.lua')
+
+--[[
+This is plugin section
+]]
+require('telescope').setup {
+  defaults = {
+    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+    color_devicons = true,
+    prompt_prefix = "   ",
+    layout_config = {
+      horizontal = {
+        prompt_position = "top",
+        preview_width = 0.55,
+      },
+      vertical = {
+        mirror = false,
+      },
+      width = 0.87,
+      height = 0.80,
+      preview_cutoff = 120,
+    },
+    mappings = {
+      i = {
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+      },
+    },
+    set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+    grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+    qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+  },
+}
+
+vim.keymap.set('n', '<leader>ds', '<cmd>Telescope treesitter<cr>', {desc = 'Telescope treesitter navigation'})
+vim.keymap.set('n', '<leader>t', "<cmd>Telescope<cr>", { desc = 'Open telescope' })
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = 'Find file' })
+vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = 'Show buffers' })
+vim.keymap.set('n', '<leader>fw', '<cmd>Telescope live_grep<cr>', { desc = 'Live grep' })
+vim.keymap.set('n', '<leader>fW', function() require('telescope').extensions.live_grep_args.live_grep_args() end, { desc = 'Live grep with args' })
+vim.keymap.set('n', '<leader>fz', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require('telescope.builtin').current_buffer_fuzzy_find({fuzzy = false})
+end, { desc = 'Fuzzily search in current buffer]' })
+vim.keymap.set('n', '<leader>m', '<cmd>Telescope marks<cr>', {desc = 'Open marks telescope'})
+vim.keymap.set('n', '<leader>r', '<cmd>Telescope registers<cr>', {desc = 'Open marks telescope'})
+--[[]]
